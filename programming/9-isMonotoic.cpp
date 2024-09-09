090c2a3d9b939f3c38c97b7eff5952a9326ffcf0
Thanapat Jonglertthiti <thanajong@outlook.com>
2024-09-09 14:58:25 +0700

    update

--- programming/9-isMonotoic.cpp
+++ programming/9-isMonotoic.cpp
@@ -1,36 +1,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-class Solution
-{
+class Solution {
 public:
-    bool isMonotonic(vector<int> &nums)
-    {
-        bool isMonotoic = true;
-        int stored = 0;
-        for (int i = 0; i < nums.size() - 1; i++)
+    bool isMonotonic(vector<int>& nums) {
+        bool increasing=true, decreasing=true;
+        for (int i = 0; i < nums.size()-1; i++)
         {
-            if (stored == 0)
-            {
-                if (nums[i + 1] - nums[i] == 1)
-                    stored = 1;
-                else if (nums[i + 1] - nums[i] == -1)
-                    stored = -1;
-                else if (nums[i + 1] - nums[i] != 0)
-                {
-                    isMonotoic = false;
-                    break;
-                }
-            }
-            if (stored == 1 || stored == -1)
-            {
-                if (nums[i + 1] - nums[i] != stored && nums[i + 1] - nums[i] != 0)
-                {
-                    isMonotoic = false;
-                    break;
-                }
-            }
+            if(nums[i+1]>nums[i]) decreasing=false;
+            else if(nums[i+1]<nums[i]) increasing=false;
         }
-        return isMonotoic;
+        return increasing||decreasing;
     }
 };
+// int main() {
+//     Solution sol;
+//     vector<int> test={2,1};
+//     bool result = sol.isMonotonic(test);
+//     cout<<result;
+// }
